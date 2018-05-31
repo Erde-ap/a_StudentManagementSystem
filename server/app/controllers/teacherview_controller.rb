7619d779efdc66e009c0c8d732acecac91c03f96
kd1186940 <kd1186940@st.kobedenshi.ac.jp>
2018-05-31 13:49:03 +0900

    修正

--- server/app/controllers/teacherview_controller.rb
+++ server/app/controllers/teacherview_controller.rb
@@ -3,7 +3,6 @@ class TeacherviewController < ApplicationController
   #データを受け取れる場合は以下２つの前の数字を消してコメントアウトしているparms[]を有効に
   @x = 1
   @allmystudent = []
-  @permission = params[:permission]
   @classes = params[:classes]
   @day = Date.today
   
@@ -31,35 +30,35 @@ class TeacherviewController < ApplicationController
     while User.find(@x).student_id == Attendance.find(y).student_id
         data = Attendance.find(y)
       
-      if data.period1 == 0
+      if data.period1 == 0 || data.period1 == 3 || data.period1 == 5
         count0 += 1
         count1 += 1
       elsif data.period1 != 9
         count1 += 1
       end
       
-     if data.period2 == 0
+     if data.period2 == 0 || data.period2 == 3 || data.period2 == 5
         count0 += 1
         count1 += 1
       elsif data.period2 != 9
        count1 += 1
      end
      
-    if data.period3 == 0
+    if data.period3 == 0 || data.period3 == 3 || data.period3 == 5
        count0 += 1
        count1 += 1
      elsif data.period3 != 9
       count1 += 1
     end
     
-    if data.period4 == 0
+    if data.period4 == 0 || data.period4 == 3 || data.period4 == 5
       count0 += 1
       count1 += 1
     elsif data.period4 != 9
       count1 += 1
     end
     
-    if data.period5 == 0
+    if data.period5 == 0 || data.period5 == 3 || data.period5 == 5
       count0 += 1
       count1 += 1
     elsif data.period5 != 9
@@ -67,6 +66,9 @@ class TeacherviewController < ApplicationController
     end
     
     y += 1
+    if y > Attendance.count
+      break
+    end
     end
     
     if count0 == 0 || count1 == 0
@@ -80,7 +82,6 @@ class TeacherviewController < ApplicationController
   def getweek
   @x = 1
   @test = []
-  @permission = params[:permission]
   @classes = params[:classes]
   @month = params[:month]
   @week = params[:week]
@@ -128,19 +129,21 @@ class TeacherviewController < ApplicationController
       date = date - 5
     end
     
-    y = Attendance.find_by(student_id: User.find(@x).student_id, year: date.strftime("%Y"), month: date.strftime("%m"), day: date.strftime("%d")).id
+    if Attendance.find_by(student_id: User.find(@x).student_id, year: date.strftime("%Y"), month: date.strftime("%m"), day: date.strftime("%d")) != nil
     
-    5.times do
-    @test << {'student_id' => User.find(@x).student_id,
-              'period1' => Attendance.find(y).period1,
-              'period2' => Attendance.find(y).period2,
-              'period3' => Attendance.find(y).period3,
-              'period4' => Attendance.find(y).period4,
-              'period5' => Attendance.find(y).period5,
-              'year' => Attendance.find(y).year,
-              'month' => Attendance.find(y).month,
-              'day' => Attendance.find(y).day}
-    y+=1
+    y = Attendance.find_by(student_id: User.find(@x).student_id, year: date.strftime("%Y"), month: date.strftime("%m"), day: date.strftime("%d")).id
+      5.times do
+      @test << {'student_id' => User.find(@x).student_id,
+                'period1' => Attendance.find(y).period1,
+                'period2' => Attendance.find(y).period2,
+                'period3' => Attendance.find(y).period3,
+                'period4' => Attendance.find(y).period4,
+                'period5' => Attendance.find(y).period5,
+                'year' => Attendance.find(y).year,
+                'month' => Attendance.find(y).month,
+                'day' => Attendance.find(y).day}
+      y+=1
+    end
     end
       
   end
