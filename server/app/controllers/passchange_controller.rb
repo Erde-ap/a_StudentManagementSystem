f274793a58a3d713aa087508931201eeeca908b6
kd1186940 <kd1186940@st.kobedenshi.ac.jp>
2018-06-06 13:09:50 +0900

    jsonを読み込むように変更

--- server/app/controllers/passchange_controller.rb
+++ server/app/controllers/passchange_controller.rb
@@ -1,17 +1,27 @@
 class PasschangeController < ApplicationController
+
+   protect_from_forgery except: :search
    def passchange
-   @student_id = params[:student_id]
-   @new_password = params[:new_password]
+   json_request = JSON.parse(request.body.read)
+
+   @student_id = json_request["student_id"]
+   @password1 = json_request["password1"]
+   @password2 = json_request["password2"]
    @status = ''
    
-   if 6 <= @new_password.length && @new_password.length <= 10
-      user = User.find_by(student_id: @student_id)
-      user.password = @new_password
-      user.save!
-      @status = '変更完了しました'
+   if @password1 == @password2
+      if 6 <= @password1.length && @password1.length <= 10
+         user = User.find_by(student_id: @student_id)
+         user.password = @password1
+         user.save!
+         @status = '変更完了しました'
+      else
+         @status = 'パスワードは6文字以上10文字以内です'
+      end
    else
-      @status = 'パスワードは6文字以上10文字以内です'
+      @status = 'パスワードが間違っています'
    end
+   
    render json: {'status' => @status}
    end
 end
