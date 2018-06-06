cfb88b134b54fa015b8ecb977ab0e1f8ed223a23
hortensian <kd1196544@st.kobedenshi.ac.jp>
2018-06-06 12:02:57 +0900

    /selo(session login)の修正

--- server/app/controllers/sessions_controller.rb
+++ server/app/controllers/sessions_controller.rb
@@ -6,17 +6,17 @@ class SessionsController < ApplicationController
     # ログインされていたらインスタンスnoticeを定義
     if session[:access_token]
       @notice = "#{session[:access_token]}でログインしています。"
+      render json:  @notice
     end
     # ログインボタン押下後にセッションが保存されるかを判断
-    if params.key?(:name) || params.key?(:password)
+    if params.key?(:student_id) || params.key?(:password)
       # nameが一致していれば代入される
-      user = User.find_by_name(params[:name])
+      user = User.find_by_student_id(params[:student_id])
       # userの中身があるかを判断しuser.authenticateでパスワードが一致しているかを返す
       if user && user.authenticate(params[:password])
         # 一致していたらセッションにユーザー名を登録
-        session[:access_token] = params[:name].hash
-        @notice = session[:access_token]
-        render json: {'session' => @notice}
+        session[:access_token] = params[:password].hash
+        render json: {'session' => session[:access_token]}
       else
         session[:access_token] = nil
         render json: {'session' => session[:access_token]}
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/test',to: 'userattendance#find'
   get '/rate',to: 'userattendance#rate'
-  get '/login', to: 'login#login'
   get '/showlist',to: 'teacherview#showtime'
   get '/showweek',to: 'teacherview#getweek'
   get '/condel',to: 'confirmation_of_delivery#delivery_not_read'
