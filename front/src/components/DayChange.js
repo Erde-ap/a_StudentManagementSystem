675f5cbb505358d0cdc7c6760e9b11ec3e860654
Kiparisv3 <kd1180043@st.kobedenshi.ac.jp>
2018-06-06 10:48:50 +0900

    Daychange作業途中

--- front/src/components/DayChange.js
+++ front/src/components/DayChange.js
@@ -1,11 +1,39 @@
 import React, {Component} from 'react'
-//import {Container, Row, Col, Form, FormGroup, Label, Input, Button} from 'reactstrap'
+import {Container, Row} from 'reactstrap'
 import Header from './Header'
+import FullCalendar from 'fullcalendar-reactwrapper';
+import '../../node_modules/fullcalendar-reactwrapper/dist/css/fullcalendar.min.css'
+//import Popup from 'react-popup'
+//import {$} from 'jquery';
+//import 'fullcalendar';
+
 
 class DayChange extends Component{
+
     render(){
         return(
-            <Header/>
+            <Container>
+                <Header/>
+                <Row className="mt-100">
+                    <FullCalendar
+                        id="user_id"
+                        header = {{
+                            left: 'prev,next today myCustomButton',
+                            center: 'title',
+                            right: 'month,basicWeek,basicDay'
+                        }}
+                        defaultDate={'2018-05-30'}
+                        navLinks= {true} // can click day/week names to navigate views
+                        editable= {true}
+                        eventLimit= {true} // allow "more" link when too many events
+                        selectable={true}
+                        locale={'ja'}
+                        dayClick={function(date,jsEvent,view) {
+                            alert(date.format('YYYY/MM/DD'))
+                        }}
+                    />
+                </Row>
+            </Container>
         )
     }
 }
