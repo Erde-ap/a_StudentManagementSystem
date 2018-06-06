3a86eb09c608b66c7d40f867fed1c156f09e4bcb
Erde-ap <mtdaier@gmail.com>
2018-06-07 04:54:16 +0900

    マージの残りを統合

--- front/src/components/Main.js
+++ front/src/components/Main.js
@@ -21,8 +21,12 @@ class Main extends React.Component {
         return (
         <main>
             <Switch>
-                <Route exact path="/" component={Login}/>
-                <Route path="/student" component={Student}/>
+            <Route exact path="/"  component={Login} />
+            <Route path="/student" component={Student} />
+            <Route path="/top" component={Top} />
+            <Route path="/changelist" component={ChangeList} />
+            <Route path="/studentlist" component={StudentList} />
+            <Route path="/daychange" component={DayChange} />
                 <Route path="/change/:id/:date" component={SendChangeView} onSubmit={this.saveSettings} />
                 <Route path="/test" component={ContactForm} onSubmit={this.saveSettings} />
             </Switch>
@@ -30,23 +34,4 @@ class Main extends React.Component {
         )
     }
 }
-
-<<<<<<< HEAD
-export default Main
-=======
-const Main = () => (
-    <main>
-        <Switch>
-            <Route exact path="/"  component={Login} />
-            <Route path="/student" component={Student} />
-            <Route path="/top" component={Top} />
-            <Route path="/changelist" component={ChangeList} />
-            <Route path="/studentlist" component={StudentList} />
-            <Route path="/daychange" component={DayChange} />
-            <Route path="/change/:date" render={props => <SendChange match={props.match} />} />
-        </Switch>
-    </main>
-);
-
 export default Main;
->>>>>>> nakama
