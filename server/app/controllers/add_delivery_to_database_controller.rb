50c6fe548d46c5683058a3816ef7f9aa6237c801
hortensian <kd1196544@st.kobedenshi.ac.jp>
2018-06-06 10:13:28 +0900

    POSTのJSONを読み込むように変更(まだ形が定かではないので未完成)

--- server/app/controllers/add_delivery_to_database_controller.rb
+++ server/app/controllers/add_delivery_to_database_controller.rb
@@ -1,18 +1,20 @@
 class AddDeliveryToDatabaseController < ApplicationController
   protect_from_forgery except: :search # searchアクションを除外
   def create
-    @student_id = params[:student_id]
-    @req_year = params[:req_year]
-    @req_month = params[:req_month]
-    @req_day = params[:req_day]
-    @req_type = params[:req_type]
-    @reason = params[:reason]
-    @req_date = params[:req_date]
-    @req_period1 = params[:period1]
-    @req_period2 = params[:period2]
-    @req_period3 = params[:period3]
-    @req_period4 = params[:period4]
-    @req_period5 = params[:period5]
+    delivery = JSON.parse(request.body.read)
+    if !delivery.brank?
+    @student_id =   delivery.student_id#delivery['student_id']#params[:student_id]
+    @req_year =     delivery.req_year#delivery['req_year']#params[:req_year]
+    @req_month =    delivery.req_month#delivery['req_month']#params[:req_month]
+    @req_day =      delivery.req_day#delivery['req_day']#params[:req_day]
+    @req_type =     delivery.req_type#delivery['req_type']#params[:req_type]
+    @reason =       delivery.reason#delivery['reason']##params[:reason]
+    @req_date = @req_year.to_s + ',' +  @req_month.to_s + ',' + @req_day.to_s
+    @req_period1 =  delivery.period1#delivery['req_period1']#params[:period1]
+    @req_period2 =  delivery.period2#delivery['req_period2']#params[:period2]
+    @req_period3 =  delivery.period3#delivery['req_period3']#params[:period3]
+    @req_period4 =  delivery.period4#delivery['req_period4']#params[:period4]
+    @req_period5 =  delivery.period5#delivery['req_period5']#params[:period5]
     stid = User.where(student_id: @student_id).pluck(:name)
 
     @newdate = Request.new(student_id: @student_id,
@@ -33,6 +35,9 @@ class AddDeliveryToDatabaseController < ApplicationController
                            approval_state: false
     )
     @newdate.save
-     render json: {'status'=> '申請完了！'}
+     render json: @newdate#{'status'=> '申請完了！'}
+    else
+      render json: {'status' => 500}
+    end
   end
 end
