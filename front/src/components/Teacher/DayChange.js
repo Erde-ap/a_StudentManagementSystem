547dace35f21fd85c77d06c5c912f06618c252f4
Kiparisv3 <kd1180043@st.kobedenshi.ac.jp>
2018-06-13 14:53:32 +0900

    何の成果も得られませんでした

--- front/src/components/Teacher/DayChange.js
+++ front/src/components/Teacher/DayChange.js
@@ -1,16 +1,17 @@
+/*
 import React, {Component} from 'react'
-import {Container, Row} from 'reactstrap'
+import {Container, Row, Popover, PopoverHeader, PopoverBody, Button} from 'reactstrap'
 import FullCalendar from 'fullcalendar-reactwrapper';
 import '../../../node_modules/fullcalendar-reactwrapper/dist/css/fullcalendar.min.css'
-import Popup from 'react-popup'
-import '../../../node_modules/react-popup/style.css'
+// import Popup from 'react-popup'
+// import '../../../node_modules/react-popup/style.css'
 import DayChangeForm from "./DayChangeForm"
-import $ from "jquery"
 
-class Prompt extends Component{
+
+class DayChange extends Component{
     constructor(props) {
         super(props);
-
+        this.toggle = this.toggle.bind(this);
         this.state = {
             events:[
                 {
@@ -30,30 +31,26 @@ class Prompt extends Component{
                     end: '2018-06-05T18:00:00'
                 }
             ],
-            value: this.props.defaultValue
+            popoverOpen: false
         };
-
-        this.onChange = (e) => this._onChange(e);
     }
 
-    componentDidUpdate(prevProps, prevState) {
-        if (prevState.value !== this.state.value) {
-            this.props.onChange(this.state.value);
-        }
-    }
-
-    _onChange(e) {
-        let value = e.target.value;
-
-        this.setState({value: value});
+    toggle() {
+        this.setState({
+            popoverOpen: !this.state.popoverOpen
+        });
     }
 
     render(){
         return(
             <Container>
                 <Row className="mt-100">
-                    <Popup/>
-                    <input type="text" placeholder={this.props.placeholder} className="mm-popup__input" value={this.state.value} onChange={this.onChange} />;
+                    <Popover placement="bottom" isOpen={this.state.popoverOpen} target="calendar_id" toggle={this.toggle}>
+                        <PopoverHeader>出席日を変更します</PopoverHeader>
+                        <PopoverBody>
+                            <DayChangeForm/>
+                        </PopoverBody>
+                    </Popover>
                     <FullCalendar
                         id="calendar_id"
                         locale={'ja'}
@@ -67,7 +64,8 @@ class Prompt extends Component{
                         events = {this.state.events}
                         selectable={true}
                         dayClick={(date) =>{
-                            Popup.alert( <DayChangeForm date={date.format('YYYY/MM/DD')} /> )
+                            this.toggle()
+                            //Popup.alert( <DayChangeForm date={date.format('YYYY/MM/DD')} /> )
                         } }
                     />
                 </Row>
@@ -76,34 +74,123 @@ class Prompt extends Component{
     }
 }
 
-/** Prompt plugin */
-Popup.registerPlugin('prompt', function (defaultValue, placeholder, callback) {
-    let promptValue = null;
-    let promptChange = function (value) {
-        promptValue = value;
+export default DayChange
+
+*/
+
+import React, {Component} from 'react'
+import {Container, Row, Popover, PopoverHeader, PopoverBody, Button} from 'reactstrap'
+
+class Example extends Component{
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            popoverOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            popoverOpen: !this.state.popoverOpen
+        });
+    }
+
+    render(){
+        return(
+            <Container>
+                <Row className="mt-100">
+                    <Button id="Popover1" onClick={this.toggle}>
+                        Launch Popover
+                    </Button>
+                    <Popover placement="bottom" isOpen={this.state.popoverOpen} target="Popover1" toggle={this.toggle}>
+                        <PopoverHeader>Header</PopoverHeader>
+                        <PopoverBody>Body</PopoverBody>
+                    </Popover>
+                </Row>
+            </Container>
+        )
+    }
+}
+
+export default Example
+/*
+
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+// import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Avatar from 'material-ui/Avatar';
+import List from 'material-ui/List/List';
+import ListItem from 'material-ui/List/ListItem';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import Popover, {PopoverAnimationVertical} from 'material-ui/Popover';
+import Chip from 'material-ui/Chip';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        };
+    }
+
+    handleToggle = (event) => {
+        this.setState({
+            open: !this.state.open,
+            anchorEl: event.currentTarget,
+        });
+    }
+
+    handleRequestClose = () => {
+        this.setState({
+            open: false,
+        });
     };
 
-    this.create({
-        title: 'What\'s your name?',
-        content: <Prompt onChange={promptChange} placeholder={placeholder} value={defaultValue} />,
-        buttons: {
-            left: ['cancel'],
-            right: [{
-                text: 'Save',
-                key: '⌘+s',
-                className: 'success',
-                action: function () {
-                    callback(promptValue);
-                    Popup.close();
-                }
-            }]
-        }
-    });
-});
+    render() {
+        return (
+            <div className="App">
+                <MuiThemeProvider>
+                    <div>
+                        <AppBar title="Title" iconClassNameRight="muidocs-icon-navigation-expand-more" showMenuIconButton={true} onLeftIconButtonTouchTap={this.handleToggle} />
+                        <Popover
+                            open={this.state.open}
+                            anchorEl={this.state.anchorEl}
+                            anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+                            targetOrigin={{horizontal: 'left', vertical: 'top'}}
+                            onRequestClose={this.handleRequestClose}
+                            animation={PopoverAnimationVertical}
+                        >
+                            <Menu>
+                                <MenuItem primaryText="Refresh" />
+                                <MenuItem primaryText="Help &amp; feedback" />
+                                <MenuItem primaryText="Settings" />
+                                <MenuItem primaryText="Sign out" />
+                            </Menu>
+                        </Popover>
+                    </div>
+                    <header>
+                        <List>
+                            <ListItem leftAvatar={<Avatar src={logo} />}>
+                                Welcome to React
+                            </ListItem>
+                        </List>
+                    </header>
 
-/** Call the plugin */
-Popup.plugins().prompt('', 'Type your name', function (value) {
-    Popup.alert('You typed: ' + value);
-});
+                    <Chip>
+                        To Get started, edit <code>src/App.js</code> and save to reload.
+                    </Chip>
 
-export default Prompt
+                </MuiThemeProvider>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+*/
