3c0188fa79e2225e3d2ec672173da6894d0dd35a
Kiparisv3 <kd1180043@st.kobedenshi.ac.jp>
2018-06-14 14:54:26 +0900

    form形式で出席日変更画面を作成

--- front/src/components/Teacher/DayChange.js
+++ front/src/components/Teacher/DayChange.js
@@ -1,64 +1,47 @@
 import React, {Component} from 'react'
-import {Container, Row} from 'reactstrap'
-import '../../../node_modules/jquery/dist/jquery.min'
-import FullCalendar from 'fullcalendar-reactwrapper';
-import '../../../node_modules/fullcalendar-reactwrapper/dist/css/fullcalendar.min.css'
-import '../../../node_modules/fullcalendar/dist/gcal.js'
-import Popup from 'react-popup'
-import '../../../node_modules/react-popup/style.css'
-import DayChangeForm from "./DayChangeForm"
-
+import {Container, Row, Col, FormGroup, Label, Input, Button} from 'reactstrap'
 
 class DayChange extends Component{
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            events: [
-                {
-                    title: '平日',
-                    dow: [1,2,3,4,5],
-                    rendering: 'background',
-                    color: '#A9D0F5'
-                },
-                {
-                    title: '休日',
-                    dow: [0,6],
-                    rendering: 'background',
-                    color: '#ff9f89'
-                },
-                {
-                    title: '創立記念日',
-                    start: '2018-06-01',
-                    rendering: 'background',
-                    color: 'red'
-                }
-            ]
+    constructor(props){
+        super(props)
+        this.state={
+            startDate:"2018/06/01"
         }
     }
     render(){
         return(
             <Container>
-                <Row className="mt-100">
-                    <Popup/>
-                    <FullCalendar
-                        googleCalendarApiKey='AIzaSyApysSuJGEdzfD-OsYkD2TtCLGryDlqLjI'
-                        id="calendar_id"
-                        locale={'ja'}
-                        header = {{
-                            left: 'prev,next,today myCustomButton',
-                            center: 'title'
-                        }}
-                        navLinks= {false} // 日付の数字部分クリック時のアクションをなくす
-                        editable= {false} // イベントをドラッグアンドドロップできなくする
-                        events = {this.state.events}
-                        selectable={true}
-                        dayClick={(date) =>{
-                            Popup.alert( <DayChangeForm date={date.format('YYYY/MM/DD')} /> )
-                        } }
-
-                    />
+                <Row className="mt-80">
+                    <Col lg={{size: 4, offset: 4}}>
+                        <h1 className="text-center">出席日の変更</h1>
+                    </Col>
                 </Row>
+                <form>
+                    <FormGroup row>
+                        <Label for="Date" lg={{size: 4, offset: 4}}>日付</Label>
+                        <Col lg={{size: 4, offset: 4}}>
+                            <Input type="date"name="date" id="exampleDate"/>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup Row>
+                        <Label for="Change" lg={{size: 4, offset: 4}}>変更</Label>
+                        <Col lg={{size: 4, offset: 4}}>
+                            <Input type="select" name="select" id="exampleSelect">
+                                <option>出席日に変更</option>
+                                <option>休日に変更</option>
+                                <option>午前休みに変更</option>
+                                <option>午後休みに変更</option>
+                            </Input>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Col lg={{size: 4, offset: 4}}>
+                            {/*<Link to="/student">*/}
+                            <Button color="primary" size="lg" block>送信</Button>
+                            {/*</Link>*/}
+                        </Col>
+                    </FormGroup>
+                </form>
             </Container>
         )
     }
